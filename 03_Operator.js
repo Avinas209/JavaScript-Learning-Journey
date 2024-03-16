404d1d7695093d1634c3ad07d6b6fdefdf531ded
Avinash209 <avinash.richin20@gmail.com>
2024-03-16 21:33:53 +0530

    Added File 03_Operator.js

---
+++ 03_Operator.js
@@ -0,0 +1,22 @@
+// Operator
+/**
+ * Operator : An operator is a mathematical symbol that produces a result based on two values
+ *  Addition : Add two numbers together or combine two strings.
+ * Subtraction:
+ * Multiplication:
+ * Division:
+ * 
+ * 
+ * Assignment : this assigns a value to a variable.
+ *      symbol(=)
+ * 
+ * Strict equality : This performs a test to see if two values are equal and of the same data type. It returns a 
+ * true/false (Boolean) result.
+ *      symbol(===)
+ * 
+ * Not, Does-not-equal : This returns the logically opposite value of what it precedes. It turns a true into a false, etc.. 
+ * When it is used alongside the Equality operator, the negation operator tests whether two values are not equal.
+ *      symbol(!, !==)
+ * 
+ * 
+ */
