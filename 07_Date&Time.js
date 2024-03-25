1ae8c90aad3a9e01379857388f9ffc69c5dc5cc1
Avinash209 <avinash.richin20@gmail.com>
2024-03-25 19:18:23 +0530

    Added Date & Time file

---
+++ 07_Date&Time.js
@@ -0,0 +1,23 @@
+// This file is about learning dates and time in javascript;
+
+let todayDate = new Date();
+// console.log(todayDate);
+// console.log(todayDate.toString());
+// console.log(todayDate.toLocaleString());
+// console.log(todayDate.toDateString());
+// console.log(todayDate.toISOString());
+// console.log(todayDate.toJSON());
+// console.log(todayDate.toLocaleDateString());
+// console.log(todayDate.toLocaleString());
+
+// console.log(typeof todayDate);
+
+
+let givenDate = new Date(2024,2,25);
+// console.log(givenDate);
+// console.log(givenDate.toLocaleString());
+// console.log(givenDate.toDateString());
+
+
+let timeStamp = Date.now();
+console.log(timeStamp);
