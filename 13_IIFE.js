10b8068d016d4f11d28be38b668ddcbc2cd8a70a
Avinash209 <avinash.richin20@gmail.com>
2024-03-28 15:59:29 +0530

    Added IIFE

---
+++ 13_IIFE.js
@@ -0,0 +1,21 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+function chai(){
+    console.log(`DB CONNECTED`);
+}
+// chai();
+
+// Another way to use this function is 
+
+(function chai(){
+    console.log(`DB CONNECTED`);
+})();            //   Pay attention to the parenthesis used in this function
+//  Question: Why we use IIFE? 
+// Answer : It is used to immediately execute the function and it is also used to protect the pollution caused by
+//  variable of global scope
+
+
+//  We must use semicolon to stop the execution of the previous IIFE before initiating 2nd IIFE function to avoid error
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`)
+})("Avinash")
