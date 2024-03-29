49c7171a7eccd752ff8876ef796a608ba4513f99
Avinash209 <avinash.richin20@gmail.com>
2024-03-29 09:23:20 +0530

    Added Switcase

---
+++ 15_Switchcase.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 4;
+switch (month) {
+    case 1:
+        console.log("Month is January")
+        break;
+    case 2:
+        console.log("Month is February")
+        break;
+    case 3:
+        console.log("Month is March")
+        break;
+    case 4:
+        console.log("Month is April")
+        break;
+    case 5:
+        console.log("Month is May")
+        break;
+
+    default:
+        console.log("Default case match")
+        break;
+}
