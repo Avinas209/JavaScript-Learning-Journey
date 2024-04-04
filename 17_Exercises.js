f7b7d7b6935117bd77672ceefc6182778b9fd9d2
Avinash209 <avinash.richin20@gmail.com>
2024-04-04 21:02:50 +0530

    Initial Commit

--- 17_Exercises.js
+++ 17_Exercises.js
@@ -1,8 +1,18 @@
 const normalObj = {}; // create a normal object
 const nullProtoObj = Object.create(null); // create an object with "null" prototype
 
-console.log(`normalObj is: ${normalObj}`); // shows "normalObj is: [object Object]"
+// console.log(`normalObj is: ${normalObj}`); // shows "normalObj is: [object Object]"
 // console.log(`nullProtoObj is: ${nullProtoObj}`); // throws error: Cannot convert object to primitive value
 
 const myObj = {}
-console.log(myObj);
+// console.log(myObj);
+
+
+var createCounter = function(n) {
+    let number = n;
+    return function() {
+        return n=n++
+    };
+};
+const counter = createCounter(10);
+console.log(counter());
